32cb6ce931d5997f05be9f676b8f871698e65262
Zeleniuc Vladislav <zeleniuc.vladislav@gmail.com>
2022-10-05 11:21:38 +0300

    Updated about section

--- src/static/about.ts
+++ src/static/about.ts
@@ -10,7 +10,7 @@ export const technologies = [
 export const aboutMe = ` I'm web developer from Chisinau, Moldova who loves tech and would like
 to help as many people as possible. My main tech focus is
 JavaScript/TypeScript and it's UI library named React, with a keen
-interest in Node and Vue. I am also passionate to web design
+interest in Node, Vue and React native. I am also passionate to web design
 because it helps to understand core concepts of UI/UX experience
 that leads to a better application overall. Beside programming I'm
-passionate to sports like football, running, cycling and racing.`;
+passionate to sports like long-distance running, cycling and football.`;
